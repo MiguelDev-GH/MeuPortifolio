5083a413a9376d29e32f0dafa08f93a1baedd2d5
MiguelDev-GH <miguelchagasmaciel@gmail.com>
2026-02-06 00:55:51 -0300

    fix:home animations and formatation on aboutme

--- baseapp/src/pages/AboutMe.jsx
+++ baseapp/src/pages/AboutMe.jsx
@@ -7,8 +7,8 @@ export const AboutMe = () => {
         <h1>About Me</h1>
         <h3>What i already used to build something:</h3>
         <div className='competenciasIcon'>
-            <img src="https://skillicons.dev/icons?i=cpp" height="40" alt="cplusplus logo"  />
             <img src="https://skillicons.dev/icons?i=c" height="40" alt="c logo"  />
+            <img src="https://skillicons.dev/icons?i=cpp" height="40" alt="cplusplus logo"  />
             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" height="40" alt="javascript logo"  />
             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" height="40" alt="python logo"  />
             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" height="40" alt="react logo"  />
